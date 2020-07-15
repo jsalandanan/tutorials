1efec8851d75635872664fec7365d739f9e3e929
jsalandanan <jsalandanan@raybeam.com>
2020-07-14 20:44:11 -0700

    hmmm 24/25

--- main.lua
+++ main.lua
@@ -8,67 +8,26 @@ function love.load()
     recursiveEnumerate('objects', object_files)
     requireFiles(object_files)
 
-    input = Input()
-    input:bind('d', 'hp_decrement')
     timer = Timer()
-    base_hp_bar = {x = 400, y = 300, w = 200, h = 50, starting_w = 200, starting_h = 50}
-    tracking_hp_bar = {x = 400, y = 300, w = 200, h = 50, starting_w = 200, starting_h = 50}
-    -- timer:tween(1, base_hp_bar, {x = 400, y = 300, w = 160, h = 50}, 'in-out-cubic')
+    circle = {radius = 24}
 
-    -- function() timer:tween(1, rect_2, {x = 400, y = 300, w = 200, h = 0}, 'in-out-cubic',
-    --     function()
-    --         timer:tween(2, base_hp_bar, {x = 400, y = 300, w = 50, h = 200}, 'in-out-cubic')
-    --         timer:tween(2, rect_2, {x = 400, y = 300, w = 200, h = 50},'in-out-cubic')
-    --     end)
-    -- end)
+    flip = true
+
+    timer:after(1, function(f)
+        if flip then timer:tween(3, circle, {radius = 96}, 'in-out-cubic') flip = false
+        else timer:tween(3, circle, {radius = 24}, 'in-out-cubic') flip = true end
+        timer:after(4, f)
+        -- love.graphics.print(circle.radius)
+    end)
+
 end
-
 function love.update(dt)
-  timer:update(dt)
-  if input:pressed('hp_decrement')
-    then bars('event_tag', 1, base_hp_bar, 'tracking_event_tag', 1.5, tracking_hp_bar, 'in-out-cubic', 4)
-  end
+    timer:update(dt)
 end
-
-function bars(hp_bar_tag, hp_bar_delay, hp_bar, tracking_event_tag, traacking_hp_bar_delay, tracking_hp_bar, mode, rate)
-    timer:tween(
-        -- hp_bar_tag,
-        hp_bar_delay,
-        hp_bar,
-        {
-            x = hp_bar.x,
-            y = hp_bar.y,
-            w = math.max(hp_bar.w - hp_bar.starting_w / rate, 0),
-            h = hp_bar.h,
-            starting_w = hp_bar.starting_w,
-            starting_h = hp_bar.starting_h
-        },
-        mode
-    )
-    timer:tween(
-        -- tracking_event_tag,
-        traacking_hp_bar_delay,
-        tracking_hp_bar,
-        {
-            x = tracking_hp_bar.x,
-            y = tracking_hp_bar.y,
-            w = math.max(tracking_hp_bar.w - tracking_hp_bar.starting_w / rate, 0),
-            h = tracking_hp_bar.h,
-            starting_w = tracking_hp_bar.starting_w,
-            starting_h = tracking_hp_bar.starting_h
-        },
-        mode
-    )
-end
-
--- set the tracking bar x and y to the 
 
 function love.draw()
-    love.graphics.setColor(255, 0, 0)
-    love.graphics.rectangle('fill', tracking_hp_bar.x - tracking_hp_bar.starting_w/2, tracking_hp_bar.y - tracking_hp_bar.starting_h/2, tracking_hp_bar.w, tracking_hp_bar.h)
-    love.graphics.setColor(255, 100, 100)
-    love.graphics.rectangle('fill', base_hp_bar.x - base_hp_bar.starting_w/2, base_hp_bar.y - base_hp_bar.starting_h/2, base_hp_bar.w, base_hp_bar.h)
-
+    love.graphics.circle('fill', 400, 300, circle.radius)
+    love.graphics.print(circle.radius)
 end
 
 function recursiveEnumerate(folder, file_list)
