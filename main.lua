0ee3504e9d906e57ba60adc897046c7c61952a81
jsalandanan <jsalandanan@raybeam.com>
2020-07-14 19:25:02 -0700

    timer exercises 22

--- main.lua
+++ main.lua
@@ -1,17 +1,32 @@
 Object = require 'libraries/classic/classic'
 Input = require 'libraries/boipushy/Input'
 Timer = require 'libraries/hump/timer'
+EnhancedTimer = require 'libraries/EnhancedTimer/EnhancedTimer'
 
 function love.load()
     local object_files = {}
     recursiveEnumerate('objects', object_files)
     requireFiles(object_files)
+
+    timer = Timer()
+    rect_1 = {x = 400, y = 300, w = 50, h = 200}
+    rect_2 = {x = 400, y = 300, w = 200, h = 50}
+    timer:tween(1, rect_1, {x = 400, y = 300, w = 0, h = 200}, 'in-out-cubic',
+    function() timer:tween(1, rect_2, {x = 400, y = 300, w = 200, h = 0}, 'in-out-cubic',
+        function()
+            timer:tween(2, rect_1, {x = 400, y = 300, w = 50, h = 200}, 'in-out-cubic')
+            timer:tween(2, rect_2, {x = 400, y = 300, w = 200, h = 50},'in-out-cubic')
+        end)
+    end)
 end
 
 function love.update(dt)
+  timer:update(dt)
 end
 
 function love.draw()
+    love.graphics.rectangle('fill', rect_1.x - rect_1.w/2, rect_1.y - rect_1.h/2, rect_1.w, rect_1.h)
+    love.graphics.rectangle('fill', rect_2.x - rect_2.w/2, rect_2.y - rect_2.h/2, rect_2.w, rect_2.h)
 end
 
 function recursiveEnumerate(folder, file_list)
